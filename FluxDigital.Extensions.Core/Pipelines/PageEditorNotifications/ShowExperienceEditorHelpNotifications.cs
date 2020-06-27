e7c83af94dc0994df08af47d7609ab70e9a29766
fluxdigital <aseabridge@flux-digital.com>
2020-06-27 03:18:53 +0100

    refactor to add values to config. add custom icon

--- FluxDigital.Extensions.Core/Pipelines/PageEditorNotifications/ShowExperienceEditorHelpNotifications.cs
+++ FluxDigital.Extensions.Core/Pipelines/PageEditorNotifications/ShowExperienceEditorHelpNotifications.cs
@@ -1,14 +1,15 @@
 ﻿using FluxDigital.Extensions.Core.Enums;
 using FluxDigital.Extensions.Core.Services;
+using Sitecore.Configuration;
+using Sitecore.Data.Fields;
 using Sitecore.Diagnostics;
 using Sitecore.Pipelines.GetPageEditorNotifications;
-using Sitecore.Shell.Applications.ContentEditor;
 
 namespace FluxDigital.Extensions.Core.Pipelines.PageEditorNotifications
 {
     public class ShowExperienceEditorHelpNotifications : GetPageEditorNotificationsProcessor
     {
-        public PageEditorNotificationsService pageEditorNotificationsService = new PageEditorNotificationsService();
+        public PageEditorNotificationsService PageEditorNotificationsService = new PageEditorNotificationsService();
 
         public override void Process(GetPageEditorNotificationsArgs arguments)
         {
@@ -18,16 +19,18 @@ namespace FluxDigital.Extensions.Core.Pipelines.PageEditorNotifications
             var contextItem = arguments.ContextItem;
             var templateName = contextItem.TemplateName;
             var templateId = contextItem.TemplateID;
-            var helpText = pageEditorNotificationsService.GetPageEditorNotification(templateId);
+            var helpText = PageEditorNotificationsService.GetPageEditorNotification(templateId);
             var PageTemplateItem = contextItem.Database.GetItem(contextItem.TemplateID);
-            bool showHelpTextForTemplate = false;
+            var showHelpTextForTemplate = false;
+            var intro = Settings.GetSetting("SauronPageInfoIntro");
+            var sauronRootItemId = Settings.GetSetting("SauronRootItemId");
+            var sauronPageTemplateFieldName = Settings.GetSetting("SauronPageTemplateFieldName");
+            var sauronRootItem = contextItem.Database.GetItem(sauronRootItemId);
+            MultilistField sauronTemplateRoots = sauronRootItem.Fields[sauronPageTemplateFieldName];
 
-            var souron = contextItem.Database.GetItem("{499F31BA-737F-4A07-955C-B4EFCFA83B07}");
-            Sitecore.Data.Fields.MultilistField souronTemplateRoots = souron.Fields["Page Template Roots"];
-
-            foreach (var path in souronTemplateRoots.GetItems())
+            foreach (var path in sauronTemplateRoots.GetItems())
             {
-                if(PageTemplateItem.Paths.FullPath.ToLower().Contains(path.Paths.FullPath.ToLower()))
+                if (PageTemplateItem.Paths.FullPath.ToLower().Contains(path.Paths.FullPath.ToLower()))
                 {
                     showHelpTextForTemplate = true;
                 }
@@ -35,9 +38,8 @@ namespace FluxDigital.Extensions.Core.Pipelines.PageEditorNotifications
 
             if (showHelpTextForTemplate)
             {
-                var intro = $"<b>This page is the {templateName} Template. Please follow the guidance below: </b>";
-                var pageEditorNotificationType = new PageEditorNotificationType() { };
-                var notification = new PageEditorNotification($"{intro}{helpText}", (PageEditorNotificationType) ExtendedPageEditorNotificationType.Information);
+                var pageEditorNotificationType = new PageEditorNotificationType();
+                var notification = new PageEditorNotification($"{string.Format(intro, templateName)}{helpText}",  (PageEditorNotificationType) ExtendedPageEditorNotificationType.Information);
                 arguments.Notifications.Add(notification);
             }
         }
--- FluxDigital.Extensions.Core/Services/PageEditorNotificationsService.cs
+++ FluxDigital.Extensions.Core/Services/PageEditorNotificationsService.cs
@@ -1,24 +1,30 @@
 ﻿using System.Linq;
+using Sitecore;
+using Sitecore.Configuration;
 using Sitecore.Data;
 
 namespace FluxDigital.Extensions.Core.Services
 {
     public class PageEditorNotificationsService
     {
-        private ID basePageConfigId = new ID("{7C384B97-0546-43BB-ADB3-DCC3A90770AF}");
+        private readonly ID basePageConfigId = new ID(Settings.GetSetting("SauronBasePageConfigId"));
+
         public string GetPageEditorNotification(ID pageTemplateId)
         {
+            var sauronPageHelpTextFieldName = Settings.GetSetting("SauronPageHelpTextFieldName");
             var helpText = string.Empty;
-            var basePageConfigItem = Sitecore.Context.Database.GetItem(basePageConfigId);
+            var basePageConfigItem = Context.Database.GetItem(basePageConfigId);
             var pageHelpTextItems = basePageConfigItem?.Axes?.GetDescendants()?.ToList();
-            if(pageHelpTextItems!=null)
+            if (pageHelpTextItems != null)
             {
-                var pageHelpTextItem = pageHelpTextItems.Find(i => i.TemplateID.ToString() == "{948B6791-82E9-42A8-87A1-2F3F6B55ED93}" && i.Fields["Template Id"].Value == pageTemplateId.ToString());
+                var sauronPageTemplateId = Settings.GetSetting("SauronPageTemplateId");
+                var pageHelpTextItem = pageHelpTextItems.Find(i => i.TemplateID.ToString() == sauronPageTemplateId && i.Fields["Template Id"].Value == pageTemplateId.ToString());
                 if (pageHelpTextItem != null)
                 {
-                    helpText = pageHelpTextItem.Fields["Help Text"].Value;
+                    helpText = pageHelpTextItem.Fields[sauronPageHelpTextFieldName].Value;
                 }
             }
+
             return helpText;
         }
     }
