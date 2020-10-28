d1ad0cb74489974c4ef7e0e29298a90b079d3b08
fluxdigital <aseabridge@flux-digital.com>
2020-10-28 02:13:00 +0000

    further updates for 9.3

--- Sauron/FluxDigital.Extensions.Sauron.Core.4.6.2/Constants/SauronConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FluxDigital.Extensions.Sauron.Core.Constants
-{
-    public class SauronConstants
-    {
-        public const string ConfigItemsCacheKey = "sauron-help-config-items";
-    }
-}
