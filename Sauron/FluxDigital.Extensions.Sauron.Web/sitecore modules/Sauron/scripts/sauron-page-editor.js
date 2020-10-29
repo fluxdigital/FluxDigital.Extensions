ee5e0a1bfdc77303a15900e7958b257bfabaf182
fluxdigital <aseabridge@flux-digital.com>
2020-10-29 00:30:01 +0000

    remove un-needed files and add updated package

--- Sauron/FluxDigital.Extensions.Sauron.Web/sitecore modules/Sauron/scripts/sauron-page-editor.js
+++
@@ -1,5 +0,0 @@
-﻿//add 'sauron-alert-info' class to parent info message div if it contains a 'sauron-page-editor-notification' div. 
-//We can then style the notification using this instead of hacking out of the box js files
-$(function () {
-    $(".sauron-page-editor-notification").closest(".alert-info").addClass("sauron-alert-info");
-});
