f0ed8258f8e9ca232105a961df612a1f1731a812
fluxdigital <aseabridge@flux-digital.com>
2021-11-27 02:11:00 +0000

    add web project with example renderings add sitecore cli and config add itemres config add nuget package config

---
+++ src/Project/Web/code/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// associated with an assembly.
+[assembly: AssemblyTitle("FluxDigital.Project.Web")]
+[assembly: Guid("1bf51ecb-6cc6-4570-beed-549c63aea788")]
